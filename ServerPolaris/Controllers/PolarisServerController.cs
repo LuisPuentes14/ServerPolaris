c8a0b9f47c044ac14ab6cffcfe3137d29efa7b51
AlejandroPuentes <alejandropuentes@wposs.com>
2023-06-14 15:47:33 -0500

    commit temporal

---
+++ ServerPolaris/Controllers/PolarisServerController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ServerPolaris.Controllers
+{
+    public class PolarisServerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ ServerPolaris/Controllers/SegurityController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ServerPolaris.Controllers
+{
+    public class SegurityController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
